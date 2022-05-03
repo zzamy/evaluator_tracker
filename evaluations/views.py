e798c1c8f91c05edc6b47f39f34371e15110c65b
zzamy <hatank1013@gmail.com>
2022-05-03 22:27:04 +0530

    page add

--- evaluations/views.py
+++ evaluations/views.py
@@ -1,3 +1,4 @@
+from math import ceil
 from django.shortcuts import render
 from . import models
 
@@ -5,11 +6,22 @@ from . import models
 
 
 def all_evaluations(request):
-    all_evaluations = models.Evaluations.objects.all()
+    # Off set size modified
+    page = int(request.GET.get("page", 1))
+    page = int(page or 1)
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    page_count = ceil(models.Evaluations.objects.count() / page_size)
+    # it is manage 1 page details list no.
+    all_evaluations = models.Evaluations.objects.all()[offset:limit]
     return render(
         request,
         "evaluations/home.html",
         context={
             "evaluations": all_evaluations,
+            "page": page,
+            "page_count": page_count,
+            "page_range": range(0, page_count),
         },
     )
