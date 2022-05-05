703b04adecd2c025dc4d6363df60d63856e8d167
zzamy <hatank1013@gmail.com>
2022-05-05 22:49:26 +0530

    Django paginator

--- evaluations/views.py
+++ evaluations/views.py
@@ -1,5 +1,5 @@
-from math import ceil
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from . import models
 
 # Create your views here.
@@ -7,21 +7,14 @@ from . import models
 
 def all_evaluations(request):
     # Off set size modified
-    page = int(request.GET.get("page", 1))
-    page = int(page or 1)
-    page_size = 10
-    limit = page_size * page
-    offset = limit - page_size
-    page_count = ceil(models.Evaluations.objects.count() / page_size)
-    # it is manage 1 page details list no.
-    all_evaluations = models.Evaluations.objects.all()[offset:limit]
+    page = request.GET.get("page", 1)
+    evaluation_list = models.Evaluations.objects.all()
+    paginator = Paginator(evaluation_list, 10)
+    evaluationss = paginator.get_page(page)
+    print(vars(evaluationss.paginator))
+
     return render(
         request,
         "evaluations/home.html",
-        context={
-            "evaluations": all_evaluations,
-            "page": page,
-            "page_count": page_count,
-            "page_range": range(0, page_count),
-        },
+        context={"evaluations": evaluationss},
     )
